1ae3ec1ea9612e369608bcd603e0c92ce9b61c9b
Mugambi Alois <mugambialois@gmail.com>
2017-03-03 12:27:20 +0300

    correct tags

--- factorial.php
+++ factorial.php
@@ -1,21 +1,20 @@
 <?php 
 
-function FirstFactorial(num) { 
+function FirstFactorial($num) { 
 
-  var factorial = 1;
+ $factorial = 1;
 
-  for (var i = 1; i <= num; i++) {  
+  for (var i = 1; i <= $num; i++) {  
     // multiply each number between 1 and num  
     // factorial = 1 * 1 = 1
     // factorial = 1 * 2 = 2
     // factorial = 2 * 3 = 6
     // factorial = 6 * 4 = 24
     // ...
-    factorial = factorial * i;
+    $factorial = $factorial * i;
   }
 
-  return factorial;
+  return $factorial;
          
 }
    
-FirstFactorial(4);    
